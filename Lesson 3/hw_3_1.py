f09f4335720825f719a1850c9de082827452b4f3
itbrains <itbrains@gmail.com>
2020-12-10 16:41:10 +0300

    Домашняя работа №1

--- Lesson 3/hw_3_1.py
+++ Lesson 3/hw_3_1.py
@@ -1,1 +1,17 @@
-#start
+#Реализовать функцию, принимающую два числа (позиционные аргументы) и выполняющую их деление. Числа запрашивать у пользователя, предусмотреть обработку ситуации деления на ноль.
+def division (arg1, arg2):
+    try:
+        print(f"Деление a на b: {arg1/arg2}")
+    except ZeroDivisionError:
+        print("На ноль делить нельзя")
+        return
+
+a = input("Введите число a: ")
+b = input("Введите число b: ")
+try:
+    a=float(a)
+    b=float(b)
+except Exception:
+    print("Необходимо ввести число")
+else:
+    division(a,b)
